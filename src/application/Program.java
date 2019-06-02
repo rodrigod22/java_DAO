28a0bad35b89b08d8d6dca0f07d559025f7d7949
Rodrigo <rodrigogat5@hotmail.com>
2019-06-02 14:23:03 -0300

    insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -34,5 +35,9 @@ public class Program {
 				System.out.println(obj);
 			}
 			
+			System.out.println("====== TEST 4: seller INSERT =====");			
+		    Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
+			sellerDao.insert(newSeller);
+			System.out.println("Inserted New id = " + newSeller.getId());
 	}
 }
