d2403ee4b45020fce96daba00f3d09e490ddee3c
Rodrigo <rodrigogat5@hotmail.com>
2019-06-02 15:29:57 -0300

    delete / update

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,8 +12,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
 					
+		Scanner sc = new Scanner(System.in);
+		
 			SellerDao sellerDao = DaoFactory.createSellerDao();			
 			System.out.println("====== TEST 1: seller findById =====");			
 			Seller seller = sellerDao.findById(3);			
@@ -39,5 +41,20 @@ public class Program {
 		    Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
 			sellerDao.insert(newSeller);
 			System.out.println("Inserted New id = " + newSeller.getId());
+			
+			
+			System.out.println("====== TEST 5: seller UPDATE =====");	
+			seller = sellerDao.findById(9);
+			seller.setName("Maria de Jesus");
+			sellerDao.update(seller);
+			System.out.println("Update completed");
+			
+			
+			System.out.println("====== TEST 6: seller DELETE=====");	
+			System.out.println("Enter id for delete teste: ");
+			int id =sc.nextInt();
+			sellerDao.deleteById(id);
+			System.out.println("Delete completed");
+			sc.close();
 	}
 }
